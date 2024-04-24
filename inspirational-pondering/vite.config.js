659a56f6f4e9088a0857b321920925fa69eaffaa
Tyler <tyler.dins88@gmail.com>
2024-04-23 19:11:14 -0700

    add code for deployment

--- inspirational-pondering/vite.config.js
+++ inspirational-pondering/vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/inspirational-quote/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
