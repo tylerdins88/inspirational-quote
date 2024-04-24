0fbcfcc5a23ce47992ef1f449e9145c90ea9efba
Tyler <tyler.dins88@gmail.com>
2024-04-23 19:49:43 -0700

    npm run build & run deploy

--- inspirational-pondering/vite.config.js
+++ inspirational-pondering/vite.config.js
@@ -4,10 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/inspirational-pondering/',
+  base: '/inspirational-quote/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
     }
   }
 })
